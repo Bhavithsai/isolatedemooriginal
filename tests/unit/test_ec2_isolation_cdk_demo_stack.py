1b131ae27ec2064723f24331b288ce177c57fca4
Bhavithsai <117253515+Bhavithsai@users.noreply.github.com>
2023-07-18 13:53:02 +0530

    Update test_ec2_isolation_cdk_demo_stack.py

--- tests/unit/test_ec2_isolation_cdk_demo_stack.py
+++ tests/unit/test_ec2_isolation_cdk_demo_stack.py
@@ -15,9 +15,9 @@
 # import aws_cdk.assertions as assertions
 # from ec2_isolation_cdk_demo.ec2_isolate import IsolateEc2Stack
 # from ec2_isolation_cdk_demo.basicinfradev import Ec2IsolationCdkDemoStack
-# VPC_ID =  'vpc-03941e18a3dde38d2'
+# VPC_ID =  'vpc-03948d2'
 # app = cdk.App()
-# ec2_isolate_stack_test = IsolateEc2Stack(app, "IsolateEc2Stack", env=cdk.Environment(account="317623606131", region="eu-central-1"), vpc_id=VPC_ID)
+# ec2_isolate_stack_test = IsolateEc2Stack(app, "IsolateEc2Stack", env=cdk.Environment(account="306131", region="eu-al-1"), vpc_id=VPC_ID)
 # ec2_isolate_stack_template = assertions.Template.from_stack(ec2_isolate_stack_test)
 
 
@@ -31,10 +31,10 @@
 #                 'Value': False 
 #             },
 #         }
-# # #    check_and_enable_termination_protection('i-00a4331ecb0be0f74')
+# # #    check_and_enable_termination_protection('i-00abe0f74')
 #     # def test_tag_ec2_instance(self):
 #     # # Create an instance.
-#     #     env=cdk.Environment(account="317623606131", region="eu-central-1")
+#     #     env=cdk.Environment(account="31131", region="eu-central-1")
 #     #     stack = Ec2IsolationCdkDemoStack(app, "ec2-isolation-cdk-demo", env= env,vpc_id=VPC_ID)
 #     #     template = assertion.Template.from_stack(stack)
 #     #         # Assert that the instance is tagged.
